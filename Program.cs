a7f855118930b275673a1f1806e9d1ad1c4463cb
Furkan Uyanık <fu.uyanik@gmail.com>
2025-06-28 20:56:01 +0300

    init Program.cs and appsettings

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using cqrs_example.FakeDB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddSingleton<FakeDB>();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
