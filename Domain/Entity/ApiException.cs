f52cf078380cb4eba30f8b339cc413410cdd17c0
Furkan Uyanık <fu.uyanik@gmail.com>
2025-07-10 01:01:46 +0300

    handle errors via global middleware

---
+++ Domain/Entity/ApiException.cs
@@ -0,0 +1,15 @@
+﻿namespace cqrs_example.Entity;
+
+public class ApiException
+{
+    public int StatusCode { get; }
+    public string Message { get; }
+    public string Details { get; }
+
+    public ApiException(int statusCode, string message, string details = null)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+}
---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Text.Json;
+using cqrs_example.Entity;
+
+namespace cqrs_example.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message, ex.StackTrace, ex.GetType());
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new ApiException(context.Response.StatusCode, ex.Message);
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
